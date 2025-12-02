d81c55294852058868b828f0e68c3f3f3fa9be61
padlichan <lili.berenyi@gmail.com>
2025-12-02 20:20:35 +0000

    Add day1 part 1

---
+++ day1.py
@@ -0,0 +1,71 @@
+class Dial:
+
+    def __init__(self):
+        self.pointer = 50
+        self.zero_count = 0
+
+    def get_pointer(self):
+        return self.pointer
+    
+    def set_pointer(self, value):
+        self.pointer = value
+        if value == 0:
+            self.zero_count += 1 
+    
+    def get_zero_count(self):
+        return self.zero_count
+
+    def turn_left(self, amount):
+        amount = amount % 100
+        value = 0
+        if self.pointer - amount < 0:
+            value = 100 + (self.pointer - amount)
+        else:
+            value = self.pointer - amount
+        self.set_pointer(value)
+
+    def turn_right(self, amount):
+        amount = amount % 100
+        value = 0
+        if self.pointer + amount >= 100:
+            value = (self.pointer + amount) - 100
+        else:
+            value = self.pointer + amount
+        self.set_pointer(value)
+
+class InputHandler:
+
+    def read_input(self, location):
+        f = open(location)
+        input = f.read()
+        return input
+    
+    @staticmethod
+    def _string_to_instruction(input):
+        return (input[0], int(input[1:]))
+    
+    def string_to_instruction_list(self, input): 
+        instruction_strings = input.split(f"\n")
+        instructions = map(InputHandler._string_to_instruction, instruction_strings)
+        return list(instructions)
+    
+def turn_dial_as_instructed(dial, instructions):
+    for i in instructions:
+        if i[0] == "L":
+            dial.turn_left(i[1])
+        else:
+            dial.turn_right(i[1])
+
+    return dial.get_zero_count()
+
+if __name__ == "__main__":
+    dial = Dial()
+    inputHandler = InputHandler()
+    input = inputHandler.read_input("inputs/day1.txt")
+    instructions = inputHandler.string_to_instruction_list(input)
+    turn_dial_as_instructed(dial, instructions)
+    password = dial.get_zero_count()
+    print(f"The password is: {password}")
+
+
+
---
+++ test_day1.py
@@ -0,0 +1,78 @@
+from day1 import Dial, InputHandler, turn_dial_as_instructed
+
+def test_turn_dial_left():
+    dial = Dial()
+    dial.turn_left(5)
+    assert dial.get_pointer() == 45
+
+def test_turn_dial_right():
+    dial = Dial()
+    dial.turn_right(5)
+    assert dial.get_pointer() == 55
+
+def test_turn_dial_left_through_0():
+    dial = Dial()
+    dial.turn_left(55) 
+    assert dial.get_pointer() == 95
+
+def test_turn_dial_left_though_100():
+    dial = Dial()
+    dial.turn_right(55) 
+    assert dial.get_pointer() == 5
+
+def test_turn_dial_left_by_1111():
+    dial = Dial()
+    dial.turn_left(1111) 
+    assert dial.get_pointer() == 39
+
+def test_turn_dial_right_by_1111():
+    dial = Dial()
+    dial.turn_right(1111) 
+    assert dial.get_pointer() == 61
+
+def test_get_zero_count_no_turns():
+    dial = Dial()
+    assert dial.get_zero_count() == 0
+
+def test_get_zero_count_after_turn():
+    dial = Dial()
+    dial.turn_left(50)
+    assert dial.get_zero_count() == 1
+
+def test_get_zero_count_after_multiple_turns():
+    dial = Dial()
+    dial.turn_left(50)
+    dial.turn_right(100)
+    dial.turn_left(100)
+    assert dial.get_zero_count() == 3
+
+def test_reads_input_from_file():
+    inputHandler = InputHandler()
+    input = inputHandler.read_input("inputs/test.txt")
+    assert input == f"L68\nL30\nR48"
+
+def test_converts_input_into_instruction_list():
+    pass
+    inputHandler = InputHandler()
+    input = inputHandler.read_input("inputs/test.txt")
+    instructions = inputHandler.string_to_instruction_list(input)
+
+    assert instructions == [("L",68), ("L",30),("R",48)]
+
+def test_turn_dial_as_instructed():
+    dial = Dial()
+    instructions = [
+        ("L",68),
+        ("L",30),
+        ("R",48),
+        ("L",5),
+        ("R",60),
+        ("L",55),
+        ("L",1),
+        ("L",99),
+        ("R",14),
+        ("L",82),
+        ]
+    
+    code = turn_dial_as_instructed(dial, instructions)
+    assert dial.get_zero_count() == 3
